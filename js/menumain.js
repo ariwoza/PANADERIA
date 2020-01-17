466841377b8f409a372f37ddc833df8c36b9c18b
ariwoza <58341730+ariwoza@users.noreply.github.com>
2020-01-17 11:46:29 -0500

    PANADERIA

--- js/menumain.js
+++ js/menumain.js
@@ -3,3 +3,13 @@ var nav = document.getElementById('nav');
 btnMenu.addEventListener('click', function(){
   nav.classList.toggle('mostrar');
 });
+
+let itemNav = document.querySelectorAll('.menu-icon')
+
+for (let i = 0; i < itemNav.length; i++) {
+  itemNav[i].addEventListener('click', () => {
+    nav.classList.toggle('mostrar');
+  })
+  
+  
+}
